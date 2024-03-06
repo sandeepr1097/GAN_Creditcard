45efa48d91c7239a19e3a02846dffba8cb113dff
sandeepr1097 <54888690+sandeepr1097@users.noreply.github.com>
2024-03-06 14:07:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,41 +20,18 @@ from PIL import Image
 st.title("Credit Card Fraud Detection Model")
 
 st.image("Credit_Card_Fraud_Logo.jpg")
-'''
 input_df = st.text_input("Please provide all the required feature details: ")
 input_df_split = input_df.split(',')
 
 submit = st.button("Submit")
-'''
-loaded_model = joblib.load('https://github.com/sandeepr1097/GAN_Creditcard/Gan_Final_Shark.sav')
-#Gan_Final_Shark_path = pickle.load(open('model.pkl','rb'))
-input_df = st.file_uploader("Upload a CSV file", type=["csv"])
-#input_df_split = input_df.split(',')
-df = pd.read_csv(input_df)
-st.write('### Uploaded CSV file:')
-st.write(df)
-submit = st.button("Submit")
+
 
 if submit:
-    prediction = loaded_model.predict(df)
+    model = pickle.load(open('model.pkl','rb'))
+    features = np.asarray(input_df_split,dtype = np.float64)
+    prediction = model.predict(features.reshape(1,-1))
     if 1 in prediction:
         st.warning('Alert: Fradulant Transaction!')
     else:
         st.warning('Alert: Legitimate Transaction!')
 
-
-
-    
-''' model = pickle.load(open('model.pkl','rb'))
-    features = np.asarray(df,dtype = np.float64)
-    st.write(features)
-    #features = Arrays.toString(features)
-    #prediction = new_model.predict(input_df)
-    prediction = model.predict(np.array(features).reshape(1, -1))
-    st.write(prediction)
-'''
-    
-
-''' Gan_Final_Shark_path = "sandeepr1097/GAN_Creditcard/model.pkl" 
-with open(Gan_Final_Shark_path, 'rb') as file:
-    new_model = pickle.load(file)'''
