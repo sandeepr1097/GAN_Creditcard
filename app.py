02b35335c5fd2fdbd0e1af90fec27b6aeec92821
sandeepr1097 <54888690+sandeepr1097@users.noreply.github.com>
2024-03-06 13:00:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ submit = st.button("Submit")
 
 if submit:
     model = pickle.load(open('model.pkl','rb'))
-    features = np.asarray(input_df_split,dtype = np.float64)
+    features = np.asarray(input_df,dtype = np.float64)
     st.write(features)
     features = Arrays.toString(features)
     #prediction = new_model.predict(input_df)
