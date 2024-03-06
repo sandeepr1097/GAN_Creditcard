f2445b24cac920448d02aedec741e3d4a072a95e
sandeepr1097 <54888690+sandeepr1097@users.noreply.github.com>
2024-03-06 14:16:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ submit = st.button("Submit")
 
 if submit:
     model = pickle.load(open('model.pkl','rb'))
-    model = load_model('Gan_Final_Shark')
+    model = load_model('https://github.com/sandeepr1097/GAN_Creditcard/Gan_Final_Shark')
     features = np.asarray(input_df_split,dtype = np.float64)
     prediction = model.predict(features.reshape(1,-1))
     if 1 in prediction:
