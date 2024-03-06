307c03ee52ed9976fd31dc2c9878d6e15fbf7e43
sandeepr1097 <54888690+sandeepr1097@users.noreply.github.com>
2024-03-06 12:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,20 +24,20 @@ input_df = st.text_input("Please provide all the required feature details: ")
 input_df_split = input_df.split(',')
 
 submit = st.button("Submit")
-'''
+
 input_df = st.file_uploader("Upload a CSV file", type=["csv"])
 df = pd.read_csv(input_df)
 st.write('### Uploaded CSV file:')
 st.write(df)
 submit = st.button("Submit")
-'''
+
 if submit:
-    Gan_Final_Shark_path = "sandeepr1097/GAN_Creditcard/model.pkl" 
+   ''' Gan_Final_Shark_path = "sandeepr1097/GAN_Creditcard/model.pkl" 
 with open(Gan_Final_Shark_path, 'rb') as file:
     new_model = pickle.load(file)
 
     '''
-    Gan_Final_Shark_path = pickle.load(open('model.pkl','rb'))'''
+    Gan_Final_Shark_path = pickle.load(open('model.pkl','rb'))
     features = np.asarray(input_df_split,dtype = np.float64)
     prediction = new_model.predict(features.reshape(1,-1))
     st.write(prediction)
