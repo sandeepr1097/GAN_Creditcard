15503d66f2551651eca0640b62bd5ef9a48a055c
sandeepr1097 <54888690+sandeepr1097@users.noreply.github.com>
2024-03-08 09:46:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ import subprocess
 package_name = 'twilio'
 subprocess.run(f'pip install {package_name}', shell=True)
 
-#from twilio.rest import Client
+from twilio.rest 
+import Client
 
 def callingfun():
   account_sid = 'AC6d06287d9416698f991295413d02c2bd'
