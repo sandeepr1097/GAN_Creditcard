1ff18939b3a0065cc18ca05b3731bed10afa8147
sandeepr1097 <54888690+sandeepr1097@users.noreply.github.com>
2024-03-08 09:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ from PIL import Image
 import subprocess
 package_name = 'twilio'
 subprocess.run(f'pip install {package_name}', shell=True)
+from twilio.rest import Client
+
 def callingfun():
   account_sid = 'AC6d06287d9416698f991295413d02c2bd'
   auth_token_old = 'f90134a5b186afdf510a0724f183c63f'
